a8647ada966b210f3c70007e99a998db85df84d6
jcfranco <migranyami@gmail.com>
2025-05-04 14:21:36 -0500

    2nd revision

--- src/app/PIA/2025/page.js
+++ src/app/PIA/2025/page.js
@@ -1,42 +1,45 @@
-"use client";
+"use client"
+import { materias as dataMaterias } from "@/data/pia2025";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
-import "@/styles/project.css";
-import {data} from "@/data/combeima";
-import { useState } from "react";
+import '@/styles/list-projects.css'
 
 export default function Pia2025() {
-  const [current, setCurrent] = useState(
-    "Hi. I will be a history infograph component"
-  );
-
-  const handleCurrent = (label) => {
-    const components = [
-      "Hi. I will be a history infograph component",
-      "Hi. I will be a journey infograph component",
-    ];
-
-    const index = data.botones.findIndex((boton) => boton === label);
-
-    const newComp = index !== -1 ? components[index] : "Lo sentimos, no encontramos ese componente";
-
-    setCurrent(newComp);
-  };
+  const pathName = usePathname();
 
   return (
     <div className="sec-container">
-      <div className="inf-container">
-        <div className="inf-description">
-          <h2>{data.title}</h2>
-          <p>{data.description}</p>
-          {data.botones.map((btn) => (
-            <button key={btn} onClick={()=>handleCurrent(btn)}>{btn}</button>
-          ))}
-        </div>
-        <div className="infografia">{current}</div>
+      <div className="card_container">
+        {dataMaterias.materias.map((item, index) => {
+          const seemore = `${pathName.replace(/\/$/, "")}/${item.redirect}`;
+          return (
+            <div className="card" key={index}>
+              <h2>{item.info.idSemestre}</h2>
+              <div className="img-container">
+                <Image src={item.cardImg.src} alt={item.cardImg.alt} fill className="cardImg" />
+              </div>
+              <div className="description">
+                <h3>{item.info.tema}</h3>
+                <p>
+                  El trabajo realizado por los estudiantes gira en torno a las
+                  actividades, actores y elementos del {item.info.tema}, abordado desde
+                  las siguientes materias:
+                </p>
+                <ul>
+                  {item.info.cursos.map((curso, i) => (
+                    <li key={i}>{curso}</li>
+                  ))}
+                </ul>
+              </div>
+              <Link href={seemore}>
+                <button>Ver este PIA</button>
+              </Link>
+            </div>
+          );
+        })}
       </div>
-      <Link className="return" href="./">
-        Volver
-      </Link>
+      <Link className="return" href="/">Volver a inicio</Link>
     </div>
   );
 }
