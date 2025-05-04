f4aa80468df19184fd1fe941bcef4dc3dc5a947d
jcfranco <migranyami@gmail.com>
2025-05-04 14:29:47 -0500

    mat.panoptico_3rd-update

--- src/app/PIA/2025/Panoptico/page.js
+++ src/app/PIA/2025/Panoptico/page.js
@@ -1,39 +1,7 @@
-"use client"
-import {data} from "@/data/sanJorge";
-import Image from "next/image";
-import { usePathname } from "next/navigation";
-import Link from "next/link";
-import '@/styles/list-projects2.css'
+import Coming from "@/components/Coming";
 
-export default function Pia2025() {
-
-const pathName = usePathname();
-
-
-  return (
-    <div className="sec-container">
-      <div className="card_container">
-      {data.map((item, index) => {
-        const seemore = `${pathName}/${item.redirect}`
-        return(
-          <div className="card" key={index}>
-            <h2>{item.projectName}</h2>
-            <div className="img-container">
-              <Image src={item.image.src} alt={item.image.alt} fill className="cardImg"/>
-            </div>
-            <div className="description">
-            <p>
-            {item.projectDescription}
-            </p>
-            </div>
-            <Link href={seemore}>
-            <button>Ver proyecto</button>
-            </Link>
-          </div>
-        )
-      })}
-    </div>
-    <Link className="return" href="/">Volver a inicio</Link>
-    </div>
-  );
+export default function Notes(){
+    return (
+        <Coming/>
+    )
 }
