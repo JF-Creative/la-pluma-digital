7aa468f30a6c8eaaf8755b38ff37e315e8d7db7c
jcfranco <migranyami@gmail.com>
2025-05-06 22:11:58 -0500

    Another modifications

--- src/data/combeima.js
+++ src/data/combeima.js
@@ -1,7 +1,7 @@
 export const data = {
-    "title":"Cañón del Combeima",
-    "description":"El Cañón del Combeima es un punto turístico clave en Ibagué. El trabajo a continuación muestra un analisis tanto de su historía, como de sus puntos turísticos.",
-    "botones":[
+    title:"Cañón del Combeima",
+    description:"El Cañón del Combeima es un punto turístico clave en Ibagué. El trabajo a continuación muestra un analisis tanto de su historía, como de sus puntos turísticos.",
+    botones:[
         "Datos históricos",
         "Turismo Vivo"
     ]
