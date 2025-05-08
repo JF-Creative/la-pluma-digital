06d799c932006692aa9a324f8695e10f428414b1
migranyami <migranyami@gmail.com>
2025-05-08 06:02:40 +0000

    site-updated

--- src/components/sites/JB_Flora-Fauna/page.js
+++ src/components/sites/JB_Flora-Fauna/page.js
@@ -70,14 +70,15 @@ function Inicio(){
   
   return(
     <>
-    <div className={styles.video_container}>Video en dron</div>
+    <div>Soy una página de inicio en construcción...</div>
+    {/* <div className={styles.video_container}>Video en dron</div>
         <div className={styles.shortcuts}>
           {shortcuts.map((shortcut) => (
             <h1 key={shortcut} className={styles.shortcut_item}>
               {shortcut}
             </h1>
           ))}
-        </div>
+        </div> */}
     </>
   )
 }
