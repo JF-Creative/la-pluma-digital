3ca2ee73e87aef2d6c690046e1ce3f8db55c1c84
migranyami <migranyami@gmail.com>
2025-05-12 07:49:51 +0000

    Website-ready-for-presentation

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,23 @@
 import Link from "next/link";
 import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>{"Hola. Soy la pagina de inicio. Estoy en construcción en este momento, pero explora el menú de la barra superior."}</div>
+    <div className={styles.indexContainer}>
+      <div className={styles.cardContainer1}>
+        <h1>¡Bienvenido a la sustenación PIA!</h1>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/cun.jpg"
+            alt="CUN Sede Ibagué"
+            fill
+            className={styles.image}
+          />
+        </div>
+        <p className={styles.description}>Este espacio está pensado en esta ocasión para la sustenación PIA Semestre A 2025. De click en el botón para empezar.</p>
+        <Link href="/PIA/2025"><button className={styles.button}>Ver proyectos PIA 2025</button></Link>
+      </div>
+    </div>
   );
 }
